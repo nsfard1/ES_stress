6765c0505f6673fabfe40d5c6c67fe5c1ca04ff9
Arshan Hashemi <arshan321@yahoo.com>
2018-03-25 22:17:22 -0700

    adding all rally tests

---
+++ Workload/rally-tests/track.py
@@ -0,0 +1,122 @@
+import random
+import os
+
+
+class QueryParamSource:
+    # We need to stick to the param source API
+    # noinspection PyUnusedLocal
+    def __init__(self, track, params, **kwargs):
+        self._params = params
+        cwd = os.path.dirname(__file__)
+        # The terms.txt file has been generated with:
+        # sed -n '13~250p' [path_to_rally_data]/geonames/documents.json | shuf | sed -e "s/.*name\": \"//;s/\",.*$//" > terms.txt
+        with open(os.path.join(cwd, "terms.txt"), "r") as ins:
+            self.terms = [line.strip() for line in ins.readlines()]
+
+    # We need to stick to the param source API
+    # noinspection PyUnusedLocal
+    def partition(self, partition_index, total_partitions):
+        return self
+
+    def size(self):
+        return 1
+
+
+class PureTermsQueryParamSource(QueryParamSource):
+    def params(self):
+        query_terms = list(self.terms)  # copy
+        query_terms.append(str(random.randint(1, 100)))  # avoid caching
+        result = {
+            "body": {
+                "query": {
+                    "terms": {
+                        "name.raw": query_terms
+                    }
+                }
+            },
+            "index": None,
+            "type": None,
+            "use_request_cache": self._params.get("use_request_cache", False)
+        }
+        return result
+
+
+class FilteredTermsQueryParamSource(QueryParamSource):
+    def params(self):
+        query_terms = list(self.terms)  # copy
+        query_terms.append(str(random.randint(1, 1000)))  # avoid caching
+        result = {
+            "body": {
+                "query": {
+                    "bool": {
+                        "must": [
+                            {
+                                "match": {
+                                    "feature_class.raw": "T"
+                                }
+                            }
+                        ],
+                        "filter": [
+                            {
+                                "terms": {
+                                    "name.raw": query_terms
+                                }
+                            }
+                        ]
+                    }
+                }
+            },
+            "index": None,
+            "type": None,
+            "use_request_cache": self._params.get("use_request_cache", False)
+        }
+        return result
+
+
+class ProhibitedTermsQueryParamSource(QueryParamSource):
+    def params(self):
+        query_terms = list(self.terms)  # copy
+        query_terms.append(str(random.randint(1, 1000)))  # avoid caching
+        result = {
+            "body": {
+                "query": {
+                    "bool": {
+                        "must": [
+                            {
+                                "match": {
+                                    "feature_class.raw": "A"
+                                }
+                            }
+                        ],
+                        "must_not": [
+                            {
+                                "terms": {
+                                    "name.raw": query_terms
+                                }
+                            }
+                        ]
+                    }
+                }
+            },
+            "index": None,
+            "type": None,
+            "use_request_cache": self._params.get("use_request_cache", False)
+        }
+        return result
+
+
+def refresh(es, params):
+    es.indices.refresh(index=params.get("index", "_all"))
+
+
+def register(registry):
+    try:
+        major, minor, patch, _ = registry.meta_data["rally_version"]
+    except AttributeError:
+        # We must be below Rally 0.8.2 (did not provide version metadata).
+        # register "refresh" for older versions of Rally. Newer versions have support out of the box.
+        registry.register_runner("refresh", refresh)
+
+    registry.register_param_source("pure-terms-query-source", PureTermsQueryParamSource)
+    registry.register_param_source("filtered-terms-query-source", FilteredTermsQueryParamSource)
+    registry.register_param_source("prohibited-terms-query-source", ProhibitedTermsQueryParamSource)
